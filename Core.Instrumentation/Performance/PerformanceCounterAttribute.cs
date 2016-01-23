28b77a5c44a3d26d5ce55debc5678e159b10ac13
xiaodoli <xiaodoli@microsoft.com>
2016-01-22 18:02:22 -0800

    temp

---
+++ Core.Instrumentation/Performance/PerformanceCounterAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Instrumentation.Performance
+{
+    using System;
+    using System.Diagnostics;
+    using PostSharp.Aspects;
+    using PostSharp.Serialization;
+
+    /// <summary>
+    /// base class for all perf counter aspects
+    /// </summary>
+    /// <remarks>this class takes of initialization and indentification</remarks>
+    [PSerializable]
+    public class PerformanceCounterAttribute : OnMethodBoundaryAspect
+    {
+        #region members
+        public string CategoryName { get; private set; }
+        public string CounterName { get; private set; }
+        public PerformanceCounterType CounterType { get; private set; }
+        public PerformanceCounterType? BaseCounterType { get; private set; }
+        [NonSerialized]
+        private PerformanceCounter performanceCounter, basePerformanceCounter;
+        #endregion
+    }
+}
--- Core.Instrumentation/Performance/TimedMethodAttribute.cs
+++ Core.Instrumentation/Performance/TimedMethodAttribute.cs
@@ -1,9 +1,49 @@
 ﻿namespace Core.Instrumentation.Performance
 {
+    using System.Linq;
+    using System.Reflection;
+    using System.Security.Cryptography;
     using PostSharp.Aspects;
+    using PostSharp.Serialization;
 
+    [PSerializable]
     public class TimedMethodAttribute : OnMethodBoundaryAspect
     {
-        
+        #region fields & props
+
+        private string eventPath;
+        private string[] parameterNames;
+        public bool IncludeArguments { get; private set; }
+        #endregion
+
+        #region ctor
+
+        public TimedMethodAttribute(bool includeArguments = true)
+        {
+            this.IncludeArguments = includeArguments;
+            // works in async mode
+            this.ApplyToStateMachine = true;
+        }
+        #endregion
+
+        #region overrides
+
+        public override void CompileTimeInitialize(MethodBase method, AspectInfo aspectInfo)
+        {
+            this.eventPath = method.DeclaringType.FullName.Replace(".", "/").Replace("+", "/") + "/" + method.Name;
+            if (this.IncludeArguments)
+            {
+                this.parameterNames = method.GetParameters().Select(p => p.Name).ToArray();
+            }
+        }
+
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            //IAnalyticsRequest request = ServerAnalytics.CurrentRequest;
+            base.OnEntry(args);
+        }
+
+        #endregion
     }
 }
